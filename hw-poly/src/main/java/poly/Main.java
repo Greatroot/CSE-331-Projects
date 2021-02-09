b7b092eae280ed86b3a152084bf6c852a06ebfb2
Greatroot <42920750+Greatroot@users.noreply.github.com>
2021-02-08 23:04:32 -0800

    Finished part one and two of hw5-part2.

---
+++ hw-poly/src/main/java/poly/Main.java
@@ -0,0 +1,75 @@
+package poly;
+
+public class Main {
+    public static void main(String[] args) {
+//        RatPoly xSq = new RatPoly(1, 2);
+//        RatPoly x1 = new RatPoly(1, 1);
+//        RatPoly x2 = new RatPoly(1, 1);
+//        RatPoly _XSqPlus2X = xSq.add(x1).add(x2);
+
+//        RatPoly xSq = new RatPoly(1, 2);
+//        RatPoly x1 = new RatPoly(1, 2);
+//        RatPoly x2 = new RatPoly(1, 1);
+//        RatPoly _2XSqPlusX = xSq.add(x1).add(x2);
+
+//        String x = RatPoly.valueOf("x").toString();
+//        System.out.println(x);
+
+//        RatPoly x = new RatPoly(1, 1);
+//        RatPoly x1 = new RatPoly(1, 0);
+//        RatPoly x2 = new RatPoly(1, 1);
+//        RatPoly x3 = new RatPoly(1, 0);
+//        RatPoly finalX = x.sub(x1).mul(x2);
+////                .add(x3);
+//        System.out.println("finalX = " + finalX);
+
+//        RatPoly x = new RatPoly(1, 0);
+//        RatPoly x1 = new RatPoly(-1, 0);
+//        RatPoly finalX = x.add(x1);
+//        System.out.println("finalX = " + finalX);
+
+//        RatPoly poly1 = RatPoly.valueOf("1*x^1+2*x^2+3*x^3+4*x^4+5*x^5");
+//        System.out.println("poly1: " + poly1);
+
+//        RatPoly poly2 = RatPoly.valueOf("6*x^2+7*x^3+8*x^4");
+//        System.out.println("poly2: " + poly2);
+//        poly2 = poly2.mul(RatPoly.valueOf("1"));
+//        System.out.println("poly2: " + poly2);
+
+
+        int[] arrayList = new int[5];
+
+        for(int i = 0; i < 5; i++)
+        {
+            arrayList[i] = (2*i) + 1;
+        }
+
+        System.out.println(arrayList[0]);
+        System.out.println(arrayList[arrayList.length / 2]);
+        System.out.println(arrayList[arrayList.length - 1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
